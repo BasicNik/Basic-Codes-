61aed551bc8b91df7e346175bdc53b03fc9be2d3
Nikhil Verma <basicnik1798@gmail.com>
2021-10-03 17:35:46 +0530

    PAS

---
+++ String Solved Problems/Print_a_string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+        char str[20];
+        cout<<"\nEnter any String :: ";
+        cin>>str;
+        cout<<"\nThe Entered String is :: [ "<<str<<" ] \n";
+
+        return 0;
+}
