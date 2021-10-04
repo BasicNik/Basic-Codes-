5a75b08a44c7f6d28f53a5bd7e45b08dc5f496f6
Nikhil Verma <90186321+BasicNik@users.noreply.github.com>
2021-10-04 20:09:27 +0530

    Create 8-Realloc.cpp

---
+++ Dynamic Memory Allocation Solved Problems/8-Realloc.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    
+    int i;
+    int *ptr = (int *)malloc(2*sizeof(int));
+    
+    if(ptr == NULL)
+    {
+        printf("Memory not available!");
+        exit(1);
+    }
+    
+    printf("Enter the two numbers: \n");
+    for(i=0; i<2; i++){
+        scanf("%d",ptr+i);
+    }
+    
+    //Memory allocation for 2 more integers
+    ptr=(int*)realloc(ptr, 4*sizeof(int));
+    if(ptr==NULL){
+        printf("Memory not available!");
+        exit(1);
+    }
+    
+    printf("Enter 2 more integers : \n");
+    for(i=2; i<4; i++)
+        scanf("%d", ptr+i);
+        
+    //printing the values onto screen
+    for(i=0; i<4; i++)
+    {
+        printf("%d \n", *(ptr+i));
+    }
+    return 0;
+}
