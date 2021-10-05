de11bc95d855c84346b382f06e44f30cbf5689f3
Basic Nik <basicnik1798@gmail.com>
2021-10-05 20:07:27 +0530

    Stack program

---
+++ Data Structures and Algorithms/Stack.cpp
@@ -0,0 +1,94 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define max 10
+
+int push(int[],int);
+int pop(int[],int);
+void disp(int[],int);
+void peak(int[],int);
+
+int main(){
+    int stack[max],top=-1;
+    int ch;
+
+    do{
+        printf("1. Push \n");
+        printf("2. Pop \n");
+        printf("3. Display \n");
+        printf("4. Quit \n");
+        scanf("%d", &ch);
+
+        switch(ch)
+        {
+            case 1:
+            {
+                top = push(stack,top);
+                }break;
+            
+            case 2:{
+                top=pop(stack,top);
+            }break;
+
+            case 3:{
+                disp(stack,top);
+            }break;
+
+            case 4:{
+                peak(stack,top);
+            }break;
+
+            case 5:{
+                exit(1);
+            }
+
+            default:
+            printf("Invalid Choice \n");
+        }
+    }while(ch<=4);
+}
+
+int push(int stack[],int top)
+{
+    int n;
+    if(top==max-1)
+        printf("Stack is full \n");
+    else{
+        printf("Enter the element to be pushed :");
+        scanf("%d",&n);
+            top++;
+            stack[top]=n;
+    }
+    return top;
+}
+
+int pop(int stack[],int top){
+    int n;
+    if(top==-1)
+        printf("Stack is empty \n");
+    else{
+        n=stack[top];
+            top--;
+            printf("%d\n",n);
+    }
+    return top;
+}
+
+void disp(int stack[],int top){
+    if(top==-1)
+        printf("Stack is empty \n");
+    else{
+        printf("Elements in the stack are :");
+        while(top>=0){
+            printf("%d \n",stack[top]);
+                top--;
+        }
+    }
+}
+
+void peak(int stack[],int top){
+    if(top==-1)
+        printf("Stack is empty \n");
+    else{
+        printf("%d", stack[top]);
+    }
+}
