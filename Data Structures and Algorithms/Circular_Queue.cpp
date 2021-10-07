5d8be9d987d8e0d56703777b9a6839b503047a18
Nikhil Verma <90186321+BasicNik@users.noreply.github.com>
2021-10-07 20:23:50 +0530

    Create Circular_Queue.cpp

---
+++ Data Structures and Algorithms/Circular_Queue.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+int queue[5];
+int front = -1, rear = -1, n=5;
+
+void insert(int val) {
+   if ((front == 0 && rear == n-1) || (front == rear+1)) {
+      cout<<"Queue Overflow \n";
+      return;
+   }
+   if (front == -1) {
+      front = 0;
+      rear = 0;
+   } else {
+      if (rear == n - 1)
+      rear = 0;
+      else
+      rear = rear + 1;
+   }
+   queue[rear] = val ;
+}
+void delete() {
+   if (front == -1) {
+      cout<<"Queue Underflow\n";
+      return ;
+   }
+   cout<<"Element deleted from queue is : "<<queue[front]<<endl;
+
+   if (front == rear) {
+      front = -1;
+      rear = -1;
+   } else {
+      if (front == n - 1)
+      front = 0;
+      else
+      front = front + 1;
+   }
+}
+void display() {
+   int f = front, r = rear;
+   if (front == -1) {
+      cout<<"Queue is empty"<<endl;
+      return;
+   }
+   cout<<"Queue elements are :\n";
+   if (f <= r) {
+      while (f <= r){
+         cout<<queue[f]<<" ";
+         f++;
+      }
+   } else {
+      while (f <= n - 1) {
+         cout<<queue[f]<<" ";
+         f++;
+      }
+      f = 0;
+      while (f <= r) {
+         cout<<queue[f]<<" ";
+         f++;
+      }
+   }
+   cout<<endl;
+}
+int main() {
+
+   int ch, val;
+   
+   do {
+        cout<<"1)Insert\n";
+        cout<<"2)Delete\n";
+        cout<<"3)Display\n";
+        cout<<"4)Exit\n";
+        cout<<"Enter choice : "<<endl;
+        cin>>ch;
+        
+        switch(ch) {
+            case 1:
+            cout<<"Input for insertion: "<<endl;
+            cin>>val;
+            insert(val);
+            break;
+
+            case 2:{
+                delete();
+            }break;
+
+            case 3:
+            display();
+            break;
+
+            case 4:
+            cout<<"Exit\n";
+            break;
+            default: cout<<"Incorrect!\n";
+        }
+    }while(ch != 4);
+return 0;
+}
